2953a06b0fad5fb1f398b5a3f52d2d5128a84053
huy2272 <huy2272@gmail.com>
2022-12-11 17:01:56 -0500

    Added _main fucntion which displays main menu Also added 3 data txt files

--- a3/src/a3/core.clj
+++ a3/src/a3/core.clj
@@ -1,7 +1,15 @@
 (ns a3.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn _main []
+  (println "*** Sales Menu ***")
+  (println "------------------")
+  (println "1. Display Customer Table")
+  (println "2. Display Product Table")
+  (println "3. Display Sales Table")
+  (println "4. Total Sales for Customer")
+  (println "5. Total Count for Product")
+  (println "6. Exit")
+  (println "Enter an option?") 
+)
+
